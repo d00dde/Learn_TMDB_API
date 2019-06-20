78743ce9fee8d1941392fceaf174ab9d1e00e0fe
D00dde <empty>
2019-06-20 12:52:09 +0300

    Переписал запрос с использованием Promise

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ window.onload = () => {
 	formSearch.addEventListener ('submit', (event) => {
 		event.preventDefault();
 		const searchText = document.querySelector('.search-text').value;
+		const container = document.querySelector('.movies');
 		let url = 'https://api.themoviedb.org/3/search/multi?';
 		url += `api_key=${options.apiKey}`;
 		if(options.language)
@@ -23,34 +24,61 @@ window.onload = () => {
 			url += `&include_adult=${options.includeAdult}`;
 		if(options.region)
 			url += `&region=${options.region}`;
-		requestGET(url);
+		container.innerHTML = 'Загрузка...';
+		GETRequest(url).then(response => {
+			renderPosters (response, container);
+		}).catch(error => {
+			console.error(error);
+		});
 	});
 }
 
-function requestGET (url) {
+function GETRequest (url) {
+	const promise = new Promise ((resolved, rejected)=> {
 	const request = new XMLHttpRequest();
 	request.open('GET', url);
-	request.addEventListener('readystatechange',() => {
-		if(request.readyState == 4) {
-			if(request.status == 200) {
-				const response = JSON.parse(request.responseText);
-				console.log(response);
-				renderMovies(response);
-			}
-			else console.error('Ошибка ответа: '+ request.status);
+	request.addEventListener('load',() => {
+		if(request.status == 200) {
+			const response = JSON.parse(request.responseText);
+			resolved(response);
 		}
+		else rejected(request.status);
+	});
+	request.addEventListener('error',() => {
+		rejected('Network error');
 	});
 	request.send();
+
+	});
+	console.log(promise);
+	return promise;
 }
 
-function renderMovies (response) {
-	const container = document.querySelector('.movies');
+function renderPosters (response, container) {
 	container.innerHTML ='';
 		response.results.forEach((movie)=>{
-			let poster = document.createElement('div');
-			poster.style.background = `url(https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path})`;
-			poster.style.width = '300px';
-			poster.style.height = '450px';
-			container.appendChild(poster);
+			createPoster(container, movie);
 		});
+}
+
+function createPoster (container, movie) {
+	const poster = document.createElement('div');
+	poster.classList.add('poster');
+	const img = document.createElement('img');
+	img.setAttribute('src', `https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`);
+	img.setAttribute('alt', movie.name || movie.title);
+	console.log(movie);
+	img.addEventListener('error', () => {
+		img.setAttribute( 'src', 'img/no_image.png');
+	});
+	img.style.width = '300px';
+	poster.appendChild(img);
+	const title = document.createElement('h3');
+	title.innerText = movie.name || movie.title;
+	poster.appendChild(title);
+	const description = document.createElement('p');
+	description.innerText = movie.overview;
+	poster.appendChild(description);
+	container.appendChild(poster);
+
 }
